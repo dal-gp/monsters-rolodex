39dda772b64095d9048ec0ab16fa0fe8924f871b
Dal S Garbuja Pun <dsgarbujapun@gmail.com>
2019-07-22 18:45:32 +1000

    Updates

---
+++ precache-manifest.9137fb3d4f3eedf7786d7ea3d7846038.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b76e7e4a940b7c49b62da43a5d486e7",
+    "url": "git%20remote%20add%20origin%20https:%20//dsgarbujapun.github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8bf85ad0c1d70bbf8ba0",
+    "url": "git%20remote%20add%20origin%20https:%20//dsgarbujapun.github.io/monsters-rolodex/static/css/main.97e08fab.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "git%20remote%20add%20origin%20https:%20//dsgarbujapun.github.io/monsters-rolodex/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "8bf85ad0c1d70bbf8ba0",
+    "url": "git%20remote%20add%20origin%20https:%20//dsgarbujapun.github.io/monsters-rolodex/static/js/main.136ff166.chunk.js"
+  },
+  {
+    "revision": "52ad0f90a5becaae82c8",
+    "url": "git%20remote%20add%20origin%20https:%20//dsgarbujapun.github.io/monsters-rolodex/static/js/runtime~main.f3b9c1be.js"
+  }
+]);
